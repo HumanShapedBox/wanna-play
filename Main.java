33f78f46a9ad869053d0da6ee6200bea53ea92eb
HumanBox <stashksenia@gmail.com>
2023-03-30 15:29:45 +0300

    units initial

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package IWantToPlayAGame;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import IWantToPlayAGame.units.BaseHero;
 import IWantToPlayAGame.units.subclass.Archer;
@@ -13,70 +14,104 @@ import IWantToPlayAGame.units.subclass.Thug;
 import IWantToPlayAGame.units.subclass.Warlock;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        // ArrayList<BaseHero> homies = new ArrayList<>();
-        // homies.add(new Archer(getName(), 1, 1));
-        // homies.add(new Archer(getName(), 3, 1));
-        // homies.add(new Archer(getName(), 6, 1));
-        // homies.add(new Knight(getName(), 2, 2));
-        // homies.add(new Knight(getName(), 4, 2));
-        // homies.add(new Knight(getName(), 5, 2));
-        // homies.add(new Knight(getName(), 7, 2));
-        // homies.add(new Monk(getName(), 5, 1));
-        // homies.add(new Feeder(getName(), 2, 1));
-        // homies.add(new Feeder(getName(), 7, 1));
 
-        // ArrayList<BaseHero> gang = new ArrayList<>();
-        // gang.add(new Crossbow(getName(), 1, 8));
-        // gang.add(new Crossbow(getName(), 3, 8));
-        // gang.add(new Crossbow(getName(), 6, 8));
-        // gang.add(new Thug(getName(), 2, 7));
-        // gang.add(new Thug(getName(), 4, 7));
-        // gang.add(new Thug(getName(), 5, 7));
-        // gang.add(new Thug(getName(), 7, 7));
-        // gang.add(new Warlock(getName(), 5, 1));
-        // gang.add(new Feeder(getName(), 2, 8));
-        // gang.add(new Feeder(getName(), 7, 8));
-
+        ArrayList<BaseHero> homies = new ArrayList<>();
+        ArrayList<BaseHero> gang = new ArrayList<>();
+        chooseYourFighter(homies, gang);
         gang.forEach(n -> System.out.println(n.getInfo()));
-        
+        System.out.println("*****");
+        homies.forEach(n -> System.out.println(n.getInfo()));
+
+        sc.close();
         //gang.forEach(n -> System.out.println(n.defeat)); // Контролирую рандом, потом уберу
+    }
+
+    private static void chooseYourFighter(ArrayList<BaseHero> good, ArrayList<BaseHero> bad) {
+        System.out.println("Введите '1' для случайной генерации персонажей\n"
+                + "Введите '2' для получения основного состава игроков");
+        // + "Введите '3' для самостоятельного формирования команды");
+        int comand = sc.nextInt();
+        if (comand == 1) {
+            randomGood(good);
+            randomBad(bad);
+        } else {
+            homies(good);
+            gang(bad);
+        }
     }
 
     private static String getName(){
         return Names.values()[new Random().nextInt(Names.values().length)].toString();
     }
 
-    private ArrayList<BaseHero> randomUnits(){
-        ArrayList<BaseHero> randomUnits = new ArrayList<>();
+    private static ArrayList<BaseHero> randomBad(ArrayList<BaseHero> bad) {
         for (int i = 0; i <= 10; i++) {
-            switch (new Random().nextInt(10)){
+            switch (new Random().nextInt(10)) {
                 case 0:
-                    randomUnits.add(new Crossbow(getName(), 1, 8));
+                    bad.add(new Crossbow(getName(), new Random().nextInt(4) + 5, new Random().nextInt(4) + 5));
                     break;
                 case 1:
-                    randomUnits.add(new Crossbow(getName(), 3, 8));
+                    bad.add(new Feeder(getName(), new Random().nextInt(4) + 5, new Random().nextInt(4) + 5));
                     break;
                 case 2:
-                    randomUnits.add(new Crossbow(getName(), 6, 8));
+                    bad.add(new Warlock(getName(), new Random().nextInt(4) + 5, new Random().nextInt(4) + 5));
                     break;
-                case 3:
-                    randomUnits.add(new Thug(getName(), 2, 7));
+                default:
+                    bad.add(new Thug(getName(), new Random().nextInt(4) + 5, new Random().nextInt(4) + 5));
                     break;
-                case 4:
-                    randomUnits.add(new Thug(getName(), 4, 7));
-                    break;
-                case 5:
-                    randomUnits.add(new Thug(getName(), 5, 7));
-                    break;
-                case 6:
-                    randomUnits.add(new Thug(getName(), 7, 7));
-                    break;
-                    randomUnits.add(new Warlock(getName(), 5, 1));
-                    randomUnits.add(new Feeder(getName(), 2, 8));
-                    randomUnits.add(new Feeder(getName(), 7, 8));
             }
         }
-        return randomUnits;
+        return bad;
+    }
+
+    private static ArrayList<BaseHero> randomGood(ArrayList<BaseHero> good) {
+        for (int i = 0; i <= 10; i++) {
+            switch (new Random().nextInt(10)) {
+                case 0:
+                    good.add(new Archer(getName(), new Random().nextInt(4) + 1, new Random().nextInt(4) + 1));
+                    break;
+                case 1:
+                    good.add(new Feeder(getName(), new Random().nextInt(4) + 1, new Random().nextInt(4) + 1));
+                    break;
+                case 2:
+                    good.add(new Monk(getName(), new Random().nextInt(4) + 1, new Random().nextInt(4) + 1));
+                    break;
+                default:
+                    good.add(new Knight(getName(), new Random().nextInt(4) + 1, new Random().nextInt(4) + 1));
+                    break;
+            }
+        }
+        return good;
+    }
+    
+    private static ArrayList<BaseHero> gang(ArrayList<BaseHero> bad) {
+        bad.add(new Crossbow(getName(), 1, 8));
+        bad.add(new Crossbow(getName(), 3, 8));
+        bad.add(new Crossbow(getName(), 6, 8));
+        bad.add(new Thug(getName(), 2, 7));
+        bad.add(new Thug(getName(), 4, 7));
+        bad.add(new Thug(getName(), 5, 7));
+        bad.add(new Thug(getName(), 7, 7));
+        bad.add(new Warlock(getName(), 5, 1));
+        bad.add(new Feeder(getName(), 2, 8));
+        bad.add(new Feeder(getName(), 7, 8));
+        return bad;
+    }
+
+    private static ArrayList<BaseHero> homies(ArrayList<BaseHero> good) {
+        good.add(new Archer(getName(), 1, 1));
+        good.add(new Archer(getName(), 3, 1));
+        good.add(new Archer(getName(), 6, 1));
+        good.add(new Knight(getName(), 2, 2));
+        good.add(new Knight(getName(), 4, 2));
+        good.add(new Knight(getName(), 5, 2));
+        good.add(new Knight(getName(), 7, 2));
+        good.add(new Monk(getName(), 5, 1));
+        good.add(new Feeder(getName(), 2, 1));
+        good.add(new Feeder(getName(), 7, 1));
+        return good;
     }
 }
